fe25a8a7540eb4e5c4f19cb2e8ef85c431c5d5d5
Abinayashree M <abinayashreem2004@gmail.com>
2026-05-20 16:53:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import streamlit as st
+from pypdf import PdfReader
 
 st.title("AI StudyMate Assistant")
 
-st.write("AI-powered Educational Document Intelligence System")
+st.subheader("Educational PDF Processing System")
 
-st.subheader("Project Initialization Successful 🚀")
+st.write("Uploaded PDF content extraction using PyPDF.")
+
+# Read PDF
+pdf = PdfReader("documents/sample.pdf")
+
+# Store extracted text
+text = ""
+
+# Loop through pages
+for page in pdf.pages:
+    text += page.extract_text()
+
+# Show extracted text
+st.text_area("Extracted PDF Content", text, height=400)
