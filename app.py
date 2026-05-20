9b0940f3d4d22b5096ab6c2313b178076a693cc5
Abinayashree M <abinayashreem2004@gmail.com>
2026-05-20 14:05:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("AI StudyMate Assistant")
+
+st.write("AI-powered Educational Document Intelligence System")
+
+st.subheader("Project Initialization Successful 🚀")
