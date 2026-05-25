314ef465d229e82d40be431bdebcc0c5f34d22e7
Abinayashree M <abinayashreem2004@gmail.com>
2026-05-25 16:49:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,17 +2,23 @@ import streamlit as st
 from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
 import faiss
 import numpy as np
+import os
 
+# Load environment variables
+load_dotenv()
+
+# Streamlit title
 st.title("AI StudyMate Assistant")
 
-st.subheader("Semantic Search & Retrieval System")
+st.subheader("AI-Powered PDF Question Answering System")
 
-# Load PDF
+# Read PDF
 pdf = PdfReader("documents/sample.pdf")
 
-# Extract text
 text = ""
 
 for page in pdf.pages:
@@ -26,20 +32,26 @@ splitter = RecursiveCharacterTextSplitter(
 
 chunks = splitter.split_text(text)
 
-# Load model
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# Embedding model
+embedding_model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
 
-# Generate embeddings
-embeddings = model.encode(chunks)
+embeddings = embedding_model.encode(chunks)
 
 embedding_array = np.array(embeddings)
 
-# Create FAISS index
-dimension = embedding_array.shape[1]
-
-index = faiss.IndexFlatL2(dimension)
+# FAISS database
+index = faiss.IndexFlatL2(
+    embedding_array.shape[1]
+)
 
 index.add(embedding_array)
+
+# Load Groq model
+llm = ChatGroq(
+    model_name="llama-3.3-70b-versatile"
+)
 
 # User input
 query = st.text_input("Ask Question From PDF")
@@ -47,18 +59,34 @@ query = st.text_input("Ask Question From PDF")
 if query:
 
     # Query embedding
-    query_embedding = model.encode([query])
+    query_embedding = embedding_model.encode([query])
 
-    # Search
-    k = 2
-
+    # Retrieve chunks
     distances, indices = index.search(
         np.array(query_embedding),
-        k
+        2
     )
 
-    st.subheader("Top Matching Results")
+    retrieved_text = ""
 
     for i in indices[0]:
-        st.write(chunks[i])
-        st.write("-------------------")
+        retrieved_text += chunks[i]
+
+    # Prompt
+    prompt = f"""
+    Answer the question based on the context below.
+
+    Context:
+    {retrieved_text}
+
+    Question:
+    {query}
+    """
+
+    # AI response
+    response = llm.invoke(prompt)
+
+    # Display answer
+    st.subheader("AI Answer")
+
+    st.write(response.content)
